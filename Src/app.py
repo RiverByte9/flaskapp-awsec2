e4de611f878ca98cc790a62333b7fa1914bfd87f
RiverByte9 <kajalsalgar909@gmail.com>
2025-11-27 15:49:01 +0530

    build- deploy

--- Src/app.py
+++ Src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1> Build & Deploy Python Web App On AWS With GitHub Action</h1><br><p>Thank you for reading....</p> '
+    return '<h1> Build & Deploy Python Web App On AWS With GitHub Action</h1><br><p>When anyone talks about DevOps, CICD is the first word that comes to mind. CICD stands for Continuous Integration and Continuous Deployment (or Delivery). </p>,<p>Thank you for reading....</p> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
